ae8e166201070a1b26cff70b184cbbf0443a190d
Olivia Pomeroy <opomeroy26@gmail.com>
2022-02-06 22:24:39 -0800

    completed mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    inputToDoItem (document.getElementById("new-task-description").value);
+    form.reset()
+    
+  });
+
 });
+
+function inputToDoItem(toDo){
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', deleteToDo)
+  btn.textContent = '♥'
+  p.textContent = `${toDo}   `
+  p.appendChild(btn);
+  console.log(p)
+  document.querySelector('#list').appendChild(p)
+}
+
+function deleteToDo(e){
+  e.target.parentNode.remove()
+}
+
+
